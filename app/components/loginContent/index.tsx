b68718869e35e58943e39ecda5ad5d24d47b6165
haru <haruki6180@gmail.com>
2024-01-16 15:42:59 +0900

    新規作成, office_id入力欄追加

--- app/components/loginContent/index.tsx
+++ app/components/loginContent/index.tsx
@@ -1,16 +1,25 @@
 "use client";
 import { signIn } from "next-auth/react";
-import { Wrapper } from "./style";
-import { Button } from "@mui/material";
+import { ButtonWrapper, Wrapper, LoginWrapper } from "./style";
+import { Button, TextField } from "@mui/material";
 
-export const LoginContent = () => {
-  return (
-    <Wrapper>
-      <span>バーチャルオフィスへようこそ^^</span>
-      <span>下のボタンからログインしてください</span>
-      <Button variant="contained" onClick={() => signIn()}>
-        ログイン
-      </Button>
-    </Wrapper>
-  );
-};
+export const LoginContent = () => (
+  <Wrapper>
+    <span>バーチャルオフィスへようこそ^^</span>
+    <span>下のボタンからログインしてください</span>
+    <ButtonWrapper>
+      <LoginWrapper>
+        <TextField
+          id="outlined-basic"
+          label="Office ID"
+          variant="outlined"
+          size="small"
+        />
+        <Button variant="contained" onClick={() => signIn()}>
+          ログイン
+        </Button>
+      </LoginWrapper>
+      <Button variant="outlined">新規作成</Button>
+    </ButtonWrapper>
+  </Wrapper>
+);
--- app/components/loginContent/style.tsx
+++ app/components/loginContent/style.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   display: flex;
+  width: 100%;
   height: 100vh;
   align-items: center;
   justify-content: center;
@@ -9,3 +10,16 @@ export const Wrapper = styled.div`
   gap: 10px;
 `;
 
+export const ButtonWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 50px;
+  align-items: flex-end;
+  justify-content: center;
+`;
+
+export const LoginWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
