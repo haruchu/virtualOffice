8152fdaea5c7d11020a49979b25f2a8727883a86
haru <haruki6180@gmail.com>
2024-01-19 13:45:15 +0900

    大体のレイアウト

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { TextField } from "@mui/material";
-import { FormWrapper, Wrapper } from "./style";
+import { FormTitle, FormWrapper, RoomCardWrapper, Wrapper } from "./style";
+import { RoomCard } from "../components/atoms/roomCard";
+import { RoomType } from "../components/atoms/room";
 
 const Create = () => {
   return (
@@ -20,8 +22,17 @@ const Create = () => {
           size="small"
         />
       </FormWrapper>
-
-      
+      <FormTitle>作成したい部屋の数を選択してください</FormTitle>
+      <RoomCardWrapper>
+        {Array(...Array(6)).map((_, i) => (
+          <RoomCard
+            key={i + 1}
+            roomType={(i + 1).toString() as RoomType}
+            count={1}
+            updateCount={() => console.log("s")}
+          />
+        ))}
+      </RoomCardWrapper>
     </Wrapper>
   );
 };
--- app/create/style.tsx
+++ app/create/style.tsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   display: flex;
   height: 100vh;
+  flex-direction: column;
+  gap: 30px;
   justify-content: center;
   align-items: center;
 `;
@@ -10,5 +12,17 @@ export const Wrapper = styled.div`
 export const FormWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  gap: 10px;
+`;
+
+export const FormTitle = styled.h2`
+  margin: 0;
+  padding: 0;
+  font-size: 20px;
+  font-weight: normal;
+`;
+
+export const RoomCardWrapper = styled.div`
+  display: flex;
   gap: 10px;
 `;
