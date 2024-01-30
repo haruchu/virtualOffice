b9d5714655858dd25d51b0d6ffffcc6a3b727ad8
haru <haruki6180@gmail.com>
2024-01-31 01:28:04 +0900

    sessionないときloginにリダイレクト

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,12 @@ import { SessionProvider, useSession } from "next-auth/react";
 const Page = () => {
   const { data: session } = useSession();
 
-  if (session) redirect("/home");
+  if (session) {
+    redirect("/home");
+  } else {
+    redirect("/login");
+  }
+
   return (
     <main
       style={{
